59e804b026628f9c55b4f5bc772b759a207127f0
QASS GmbH <ymalakhova@qass.office>
2022-05-30 14:01:15 +0200

    fetc

--- src/components/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/components/MovieDetailsPage/MovieDetailsPage.jsx
@@ -13,20 +13,27 @@ export const MovieDetailsPage = () => {
 
     return (
         movie &&
-        <>        
-            {console.log(movie.genres)}
+        <>   
             
             <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} width="10" alt="" />
 
-            <h2>{movie.original_title} <span>({movie.release_date.split('-')[0]})</span></h2>
-            <p><span>User score: </span>{`${movie.vote_average * 10} %`}</p>
+            <h2>{movie.original_title ? movie.original_title : movie.original_name} 
+                <span>({movie.release_date ? movie.release_date.split('-')[0] : movie.first_air_date.split('-')[0]})</span>
+            </h2>
+
+            <p><span>User score: </span>
+                {`${movie.vote_average * 10} %`}
+            </p>
             
             <h3>Overview</h3>
             <p>{movie.overview}</p>
             
-            <h3>Genres</h3>            
-            <p>{movie.genres.map((genre) => genre.name).join(', ')}</p>
-
+            { movie.genres && 
+            <>
+                <h3>Genres</h3>            
+                <p>{movie.genres.map((genre) => genre.name).join(', ')}</p>
+            </>                             
+            }
 
             <Outlet />
         </>        
